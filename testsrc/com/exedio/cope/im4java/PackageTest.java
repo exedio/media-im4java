7656a4a911ee16e1cdb566cee87ace0b724eb6e5
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2013-01-18 10:10:46 +0000

    rename PackageUtilTest to PackageTest
    git-svn-id: https://svn.exedio.com/cope/im4java/trunk@25229 e7d4fc99-c606-0410-b9bf-843393a9eab7

--- testsrc/com/exedio/cope/im4java/PackageTest.java
+++ testsrc/com/exedio/cope/im4java/PackageTest.java
@@ -22,7 +22,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class PackageUtilTest extends TestCase
+public class PackageTest extends TestCase
 {
 	public static Test suite()
 	{
